23f3d7ff10af4bf94f0b9c2bcc52343f122edd84
John O'kane <jtommyok8@gmail.com>
2017-03-23 15:15:20 -0500

    added methods to serialize and deserialize the weight matrices

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,6 @@
 import numpy
 import random
+import re
 # A generic layer of neurons for a neural network, with dense connections.
 class NeuronLayer():
 	"""A generic layer of neurons in a neural network"""
@@ -28,3 +29,37 @@ class NeuronLayer():
 	def setWeightMatrix(self, matrix):
 		self.weightMatrix = matrix
 
+class NeuralNetwork():
+
+	def _init_(self, inputSize=5, layerSizes={}, includeBiasNode=True, weightFileFile=None):  # Use layerSizes and weightFile to signal desire to generate or load the matrix, respectively.
+		if weightFile != None:
+			f = open(weightFile, 'rb')
+			numLayers = int(f.readline().rstrip())
+			for l in range(numLayers):
+				w,h = [int(x) for x in f.readline().]
+		else if layerSizes != {}:
+			self.neuralLayers = {}
+			width = inputSize
+			for height in layerSizes:
+				self.neuralLayers.append(NeuronLayer(height=height, width=width, includeBiasNode=includeBiasNode, generateWeight=True))
+				width = height + (includeBiasNode) ? 1 : 0
+
+	def serializeMatrix(matrix):
+		rows, columns = matrix.size
+		serialMatrix = "{(" + rows +"," columns + ")"
+		for r in range(rows):
+			for c in range(columns):
+				serialMatrix += matrix[r,c]
+
+
+	def deSerializeMatrix(matrixString):
+		dimensions = re.match(r"\{\((\d*),(\d*)\)", matrixString)
+		rows = int(dimensions.group(1))
+		columns = int(dimensions.group(2))
+		matrix = numpy.zeros((rows, columns))
+		numList = re.match(r"\{\(\d*,\d*\)((?:\d*,?)*)\}")
+		count = 0
+		for num in numList.split(","):
+			matrix[int(count / columns), count % columns] = int(num)
+			count = count + 1
+		return matrix
